ad44e601346d2915bccfeaccbee975bc1aff101b
Sam Murray <sam@pmurray.com>
2018-01-27 09:35:17 -0500

    Switch intake controls

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -18,7 +18,7 @@ void Robot::Update(int joystickSlot)
     driveT.Update(joystickGetAnalog(joystickSlot,L_MOVE),joystickGetAnalog(joystickSlot,R_MOVE));
     liftL.Update(joystickGetDigital(joystickSlot,5,JOY_UP),joystickGetDigital(joystickSlot,5,JOY_DOWN));
     liftR.Update(joystickGetDigital(joystickSlot,6,JOY_DOWN),joystickGetDigital(joystickSlot,6,JOY_UP));
-    intake.Update(joystickGetDigital(joystickSlot,7,JOY_DOWN),joystickGetDigital(joystickSlot,7,JOY_UP));
+    intake.Update(joystickGetDigital(joystickSlot,7,JOY_UP),joystickGetDigital(joystickSlot,7,JOY_DOWN));
     claw.Update(joystickGetDigital(joystickSlot,8,JOY_RIGHT));
     secondaryLift.Update(joystickGetDigital(joystickSlot,8,JOY_DOWN),joystickGetDigital(joystickSlot,8,JOY_UP));
 }
